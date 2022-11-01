b2aec3203c3dd11ea1ab146428d140d72c05db3d
Max <maxfyk@gmail.com>
2022-11-01 14:33:41 +0800

    0.0.0.0

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting to trigger gunicorn"
 #gunicorn api:app -w 1 --threads 2 -b 0.0.0.0:8000 &
-python web.py
+python /app/web.py
 echo "Starting Nginx Service"
 nginx -g 'daemon off;'

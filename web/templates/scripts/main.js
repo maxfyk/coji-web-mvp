87f888c5f574a8e3445596cdc81a0e096fbf3875
Max <maxfyk@gmail.com>
2022-11-20 17:23:16 +0800

    crossorigin="anonymous"

--- web/templates/scripts/main.js
+++ web/templates/scripts/main.js
@@ -11,7 +11,6 @@ let model;
 
 /*permissions*/
 $(function () {
-
     navigator.geolocation.getCurrentPosition(function (pos) {
         lat = pos.coords.latitude;
         lon = pos.coords.longitude;

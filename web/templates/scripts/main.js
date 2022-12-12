19872bf49cdb61effc48ae8bbad170f3a902ce7d
Max <maxfyk@gmail.com>
2022-12-12 20:55:17 +0800

    rm

--- web/templates/scripts/main.js
+++ web/templates/scripts/main.js
@@ -142,7 +142,6 @@ async function scanCode() {
             } else {
                 failedToScan = false;
                 $('#index-iframe', window.parent.document).attr('src', 'data-preview/' + resp['code-id']);
-                window.parent.history.pushState({}, 'AR', 'data-preview/' + resp['code-id']);
                 $(".usage-help").text('Loading...');
 
             }
